1874faffd08d6f49f60fc598e3705cbf60ed0126
gosurvashi29 <urvashigoswami350@gmail.com>
2025-03-28 17:04:19 +0530

    first deploy

--- util/database.js
+++ util/database.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-/*const Sequelize  = require("sequelize");
+const Sequelize  = require("sequelize");
 
 const sequelize= new Sequelize(process.env.DB_NAME, process.env.DB_USER , process.env.DB_PASSWORD,{
     
@@ -7,14 +7,14 @@ const sequelize= new Sequelize(process.env.DB_NAME, process.env.DB_USER , proces
     
     host: process.env.RDS_ENDPOINT
 })
-*/
 
 
-const Sequelize  = require("sequelize");
+
+/*const Sequelize  = require("sequelize");
 
 const sequelize= new Sequelize('job_app', 'root' , 'admin1234',{
     dialect : 'mysql',
     host: 'localhost'
 })
-
+*/
 module.exports=sequelize;
